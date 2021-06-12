32aaeec8cbea1f96a497a3c27e9d98c280520975
April Ma <54913304+april5622@users.noreply.github.com>
2021-06-12 16:41:20 -0700

    setting up express and routers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,67 @@
 require("dotenv").config();
 const express = require('express');
-
 const app = express();
+
+// MIDDLEWARE
+
+app.use(express.json())
+
+// GET ALL restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(201).json({
+        status: "success",
+        data: { 
+            restaurant: ["Sushi Sam", "Ultra Sushi"],
+        },
+        
+    })
+});
+
+// GET a restaurant
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: { 
+            restaurant: "Sushi Sam",
+        },
+        
+    })
+});
+
+// POST Restaurants
+
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data: { 
+            restaurant: "Sushi Sam",
+        },
+        
+    });
+})
+
+//UPDATE restaurants
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    res.status(201).json({
+        status: "success",
+        data: { 
+            restaurant: "Sushi Sam",
+        },
+        
+    });
+});
+
+// DELETE restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "success",
+    });
+});
 
 const port = process.env.PORT || 4001;
 app.listen(port, () => {
