8a7b9451bad815b6b23c51b41a47d5285af7b6f5
April Ma <54913304+april5622@users.noreply.github.com>
2021-06-14 22:46:22 -0700

    setting up contextapi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePafe";
 import RestaurantdetailPage from "./routes/RestaurantdetailPage";
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 function App() {
     return (
-        <div className="container">
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
-                    <Route exact path="/restaurants/:id" component={RestaurantdetailPage}/>
-                </Switch>
-            </Router>
-        </div>
+        <RestaurantsContextProvider>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+                        <Route exact path="/restaurants/:id" component={RestaurantdetailPage}/>
+                    </Switch>
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
     )
 }
 
